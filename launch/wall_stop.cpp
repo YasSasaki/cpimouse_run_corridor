45ec9f2375c8d2da2384a1cde55cd0801d1ed43f
Yasuhiro Sasaki <yasuhiro_sasaki@denso.co.jp>
2018-11-29 08:00:06 +0000

    corridor

---
+++ launch/wall_stop.cpp
@@ -0,0 +1,11 @@
+#include "ros/ros.h"
+#include <ros/package.h>
+#include "raspimouse_ros_2/lightSensorValue.h"
+
+#include "signal.h"
+
+int main(int argc, char **agrv)
+{
+	init(argc, argv, "wall_stop");
+	exit(0);
+}
